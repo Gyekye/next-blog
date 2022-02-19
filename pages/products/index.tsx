62b839c1a01c0b00b6511b572be169d00eee359a
Gyekye <mrgyekye740@gmail.com>
2022-02-19 13:10:39 +0000

    feat: fetch data with SWR
    Signed-off-by: Gyekye <mrgyekye740@gmail.com>

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,40 @@
 import {NextPage} from 'next';
+import useSWR, {SWRResponse} from 'swr';
+
+
+type Product = {
+	id: number;
+	name: string;
+	price: number;
+};
 
 const Products: NextPage = (): JSX.Element => {
+
+	//* Client Side Data fetching with SWR
+	//? https://swr.vercel.app/
+	//? Create a fetcher function that will be called whenever the data is needed.
+
+	const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+	//? Data returns a product array, error is string
+	const {data, error}: SWRResponse<Product[], string> = useSWR('http://localhost:3000/products', fetcher);
+
 	return (
 		<>
-			<h1 >Product 1</h1 >
-			<h1 >Product 2</h1 >
-			<h1 >Product 3</h1 >
+			{
+				error && <div >Failed to load</div >
+			}
+			{
+				data &&
+				<div >
+					{data.map((product: Product) =>
+						<div key={product.id} >
+							<p>{product.name} - {product.price}</p
+							><br />
+						</div >
+					)}
+				</div >
+			}
 		</>
 	);
 
